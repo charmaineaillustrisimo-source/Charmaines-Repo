a076c535524a03a922d83d51b1260913d2617638
Sheanne <Sheanne@Hillcat>
2026-04-13 16:26:37 +0800

    Sheanne

--- CarRentalSystem/src/carrentalsystem/CarRentalSystem.java
+++ CarRentalSystem/src/carrentalsystem/CarRentalSystem.java
@@ -14,7 +14,7 @@ public class CarRentalSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Sheanne Gwapo");
+        
     }
     
 }
