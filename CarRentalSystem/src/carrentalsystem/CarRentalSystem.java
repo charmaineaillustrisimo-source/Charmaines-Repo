aa5697e7a3e984654cf3f49940104e2d4b6ebeb6
Charmaine A. Illustrisimo <charmaineaillustrisimo@gmail.com>
2026-04-13 17:01:05 +0800

    Reset Main Class

--- CarRentalSystem/src/carrentalsystem/CarRentalSystem.java
+++ CarRentalSystem/src/carrentalsystem/CarRentalSystem.java
@@ -14,9 +14,8 @@ public class CarRentalSystem {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         // TODO code application logic here
-        System.out.println("Hello World!");
+        
     }
     
 }
